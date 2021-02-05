b7160a49f3b3a52c505297c03c49ac3dda9a1901
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-05 13:20:22 -0500

    Setup Amazon Clone Firebase Backend

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOVycIy8X0IX205D7CaxrcDkorI_bftC8",
+  authDomain: "clone-9babb.firebaseapp.com",
+  projectId: "clone-9babb",
+  storageBucket: "clone-9babb.appspot.com",
+  messagingSenderId: "632628525793",
+  appId: "1:632628525793:web:fdb3e80cc98edc1e9155a0",
+  measurementId: "G-0362JN2ZBG",
+};
