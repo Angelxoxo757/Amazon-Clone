fe2960fe0e3a32fe78104c3388d5909a99967730
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-12 13:12:25 -0500

    Working on Amazon Clone Home Row Sections

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,22 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <p>Home Screen</p>
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3673_SVOD_ValentinesDay/RB-3673_SVOD_ValentinesDay_v2_1500x600._CB661029042_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+      </div>
     </div>
   );
 }
