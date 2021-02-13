221b6ff0bd1e197f70fe787d03728dfc06be8574
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-12 22:38:22 -0500

    Working on Amazon Clone Checkout Page and updated npm

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { Button } from "@material-ui/core";
 
 function Subtotal() {
   return (
     <div className="subtotal">
-      <h1>Subtotal</h1>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):
+              <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeperator={true}
+        prefix={"$"}
+      />
+      <Button>Proceed to Checkout</Button>
     </div>
   );
 }
