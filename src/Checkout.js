a7ffcc9b6eb3d92939ce98cbd8828825677fd3e4
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-13 14:22:08 -0500

    Working on Amazon Clone Checkout Product Page Section React Context API

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
@@ -13,6 +14,7 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
+          <CheckoutProduct />
         </div>
       </div>
       <div className="checkout__right">
