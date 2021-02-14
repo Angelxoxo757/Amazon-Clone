246785f1706e921f17001c455cea92e93870b23f
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-14 15:55:27 -0500

    Working on Amazon Clone Login Front-End Section React Context API

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div className="login"></div>;
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Amazon-Logo.png"
+          alt=""
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By signing in you agree to Amazon’s Clone conditions of use & sale.
+          Please see our own privacy notice, our cookies notice and our
+          Internet-based ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
